39efa0d18d02619c71d1f9dbcee3b8996cd4538f
Dmitry Fuks <difuks@gmail.com>
2022-10-11 02:18:55 +0300

    chore: remove i18n header usage

--- entries/admin/src/shared/api/authApi.ts
+++ entries/admin/src/shared/api/authApi.ts
@@ -1,20 +1,12 @@
-import { getApi, Client } from '@fuks-ru/auth-client';
+import { getApi } from '@fuks-ru/auth-client';
 import { getBaseQuery } from '@fuks-ru/common-frontend';
 import { message } from 'antd';
 import qs from 'qs';
 
 import { authBackendUrl, authFrontendUrl } from 'admin/shared/config';
 
-const getAuthApi = async (): Promise<Client> => {
-  const authApi = await getApi(authBackendUrl);
-
-  authApi.defaults.headers.common.i18next = 'ru-RU';
-
-  return authApi;
-};
-
 export const authBaseQuery = getBaseQuery({
-  getClient: getAuthApi,
+  getClient: () => getApi(authBackendUrl),
   onForbidden: () => {
     window.location.assign(
       `${authFrontendUrl}?${qs.stringify({
--- entries/admin/src/shared/api/mainApi.ts
+++ entries/admin/src/shared/api/mainApi.ts
@@ -1,20 +1,12 @@
-import { getApi, Client } from '@fuks-ru/esenin-family-client';
+import { getApi } from '@fuks-ru/esenin-family-client';
 import { getBaseQuery } from '@fuks-ru/common-frontend';
 import { message } from 'antd';
 import qs from 'qs';
 
 import { authFrontendUrl, backendUrl } from 'admin/shared/config';
 
-const getMainApi = async (): Promise<Client> => {
-  const mainApi = await getApi(backendUrl);
-
-  mainApi.defaults.headers.common.i18next = 'ru-RU';
-
-  return mainApi;
-};
-
 export const mainBaseQuery = getBaseQuery({
-  getClient: getMainApi,
+  getClient: () => getApi(backendUrl),
   onForbidden: () => {
     window.location.assign(
       `${authFrontendUrl}?${qs.stringify({
